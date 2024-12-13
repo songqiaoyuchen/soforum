4e5ede5022fcecffc09a888f4c13368b9a495029
SQ Yuchen <songqiaoyuchen@gmail.com>
2024-12-13 14:56:36 +0800

    Update

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+import './App.css';
+function App() {
+    return (_jsx("h1", { children: "hello world" }));
+}
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+function Navbar() {
+    return (_jsx("h1", { children: "uh" }));
+}
+export default Navbar;
