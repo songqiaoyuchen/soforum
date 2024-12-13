0771ed825aa90fb19525adfef46264eb537c73f8
SQ Yuchen <songqiaoyuchen@gmail.com>
2024-12-14 01:57:28 +0800

    trivial styling

--- src/components/Dialog/Dialog.style.ts
+++ src/components/Dialog/Dialog.style.ts
@@ -1,11 +1,25 @@
 import { styled } from '@mui/material/styles';
 import { Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 
+export const StyledDialog = styled(Dialog) ({
+})
+
+export const StyledDialogContent = styled(DialogContent) ({
+  '&.MuiDialogContent-root': {
+    paddingTop: '18px',  // This will increase the specificity and apply the style properly
+  },
+})
+
 export const StyledDialogActions = styled(DialogActions)({
-  paddingRight:  '24px',
-  paddingBottom: '20px'
+  paddingRight: '24px',
+  paddingBottom: '24px'
 });
 
 export const StyledDialogTitle = styled(DialogTitle)({
-  paddingLeft:  '25px',
+  paddingTop: '28px',
+  paddingLeft: '25px',
+  paddingBottom: '0px'
 });
+
+
+
--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,5 +1,8 @@
-import { Dialog, DialogTitle, DialogContent, Button, TextField, Box } from '@mui/material';
-import { StyledDialogTitle, StyledDialogActions } from './Dialog.style';
+import {useState} from 'react';
+import { Button, TextField, Box } from '@mui/material';
+import { StyledDialog, StyledDialogContent, StyledDialogTitle, StyledDialogActions } from './Dialog.style';
+import frogLook from '../../assets/images/frog-look.png';
+import frogClose from '../../assets/images/frog-close.png'
 
 interface DialogProps {
     open: boolean;
@@ -8,38 +11,79 @@ interface DialogProps {
   }
 
 function CustomDialog(props: DialogProps) {
+
+    const [typingPassword, setTypingPassword] = useState(false);
+
+    function toggleFrog() {
+        setTypingPassword(!typingPassword);
+    }
+
     return (
-        <Dialog {...props}>
-            <StyledDialogTitle>{props.type === "login" ? "Login" : "Signup"}</StyledDialogTitle>
-            <DialogContent>
-            <Box display="flex" flexDirection="column" gap={2} width={300}>
-                {/* Username Field */}
-                <TextField
-                label="Username"
-                type="text"
-                variant="outlined"
-                fullWidth
-                />
-                {/* Password Field */}
-                <TextField
-                label="Password"
-                type="password"
-                variant="outlined"
-                fullWidth
-                />
+        <StyledDialog {...props} fullWidth>
+            <Box display="flex" justifyContent="center">
+                <Box  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'flex-end',
+                    height: '300px',
+                }}>
+                    <img
+                    src={typingPassword ? frogClose : frogLook}
+                    alt="dialog image"
+                    style={{ width: "100px", 
+                             height: "100px", 
+                             borderRadius: "8px",
+                             transform: 'scaleX(-1)' }}
+                    />
+                </Box>
+                
+                <Box width="100%" maxWidth="70%">
+                    <StyledDialogTitle>{props.type === "login" ? "Login" : "Signup"}</StyledDialogTitle>
+                    <StyledDialogContent>
+                    <Box display="flex" flexDirection="column" gap={2} width="100%">
+                        {/* Username Field */}
+                        <TextField
+                        label="Username"
+                        type="text"
+                        variant="outlined"
+                        fullWidth
+                        />
+                        {/* Password Field */}
+                        <TextField
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                        onFocus={toggleFrog}
+                        onBlur={toggleFrog}
+                        fullWidth
+                        />
+                    </Box>
+                    </StyledDialogContent>
+                    <StyledDialogActions>
+                        {/* Cancel Button */}
+                        <Button onClick={(e) => props.onClose(e, 'trivial')} color="secondary">
+                            Cancel
+                        </Button>
+                        {/* Login / Signup Button */}
+                        <Button onClick={(e) => props.onClose(e, 'trivial')} variant="contained" color="primary">
+                            {props.type}
+                        </Button>
+                    </StyledDialogActions>
+                </Box>
+                <Box  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'flex-end',
+                    height: '300px', 
+                }}>
+                    <img
+                    src={typingPassword ? frogClose : frogLook}
+                    alt="dialog image"
+                    style={{ width: "100px", height: "100px", borderRadius: "8px" }}
+                    />
+                </Box>
             </Box>
-            </DialogContent>
-            <StyledDialogActions>
-                {/* Cancel Button */}
-                <Button onClick={(e) => props.onClose(e, 'trivial')} color="secondary">
-                    Cancel
-                </Button>
-                {/* Login / Signup Button */}
-                <Button onClick={(e) => props.onClose(e, 'trivial')} variant="contained" color="primary">
-                    {props.type}
-                </Button>
-            </StyledDialogActions>
-      </Dialog>
+      </StyledDialog>
     );
 }
 
